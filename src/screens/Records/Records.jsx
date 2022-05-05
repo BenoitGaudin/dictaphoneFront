b1d0c2853876c739968f3dd68dc44392bfe36d7a
benoitgaudin <benoitgaudin44@gmail.com>
2022-05-06 00:09:43 +0200

    wip

--- src/screens/Records/Records.jsx
+++ src/screens/Records/Records.jsx
@@ -1,74 +1,59 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import NavbarRecords from '../../components/NavbarRecords/NavbarRecords';
 import './Records.css'
 
 
 
 const Records = () => {
+
+    const [records, setRecords] = useState([]);
+
+    const b64toBlob = (base64, type = 'application/octet-stream') =>
+        fetch(`data:${type};base64,${base64}`).then(res => res.blob())
+
+    let getRecords = async () => {
+        let response = await axios.get('http://localhost:3000/records')
+        let records = response.data
+
+        console.log('records:', records)
+
+        let recordsAsMp3 = records.map(async r => {
+            console.log('converting to blob', r)
+
+            let blob = await b64toBlob(r.buffer)
+            return {
+                filename: r.filename,
+                blob: blob
+            }
+        })
+
+        setRecords(recordsAsMp3)
+    }
+
+    useEffect(() => {
+        getRecords();
+    }, []);
+
+    let recordsElems = records.map(r => (
+        <tr>
+            <th scope="row "></th>
+            <td>{r.filename}</td>
+            <td>BOUTON PLAY</td>
+            <td>DELETE</td>
+            <td><a href={r.blob}>DOWNLOAD</a></td>
+            <td>CONTACT</td>
+        </tr>
+    ))
+
     return (
         <div>
-
             <NavbarRecords />
-
             <table class="table table border border-dark">
-
                 <tbody>
-                    <tr>
-                        <th scope="row  "></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-
-                    </tr>
-                    <tr>
-                        <th scope="row  "></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-                    </tr>
-                    <tr>
-                        <th scope="row"></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-                    </tr>
-                    <tr>
-                        <th scope="row  "></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-                    </tr>
-                    <tr>
-                        <th scope="row  "></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-                    </tr>
-                    <tr>
-                        <th scope="row"></th>
-                        <td>record 001</td>
-                        <td>BOUTON PLAY</td>
-                        <td>DELETE</td>
-                        <td>DOWNLOAD</td>
-                        <td>CONTACT</td>
-                    </tr>
-
-
+                    {recordsElems}
                 </tbody>
-
             </table>
-
-
         </div>
     );
 };
