9b843f83b9f31216fe83b85a26d4ad6f4f8f3eab
benoitgaudin <benoitgaudin44@gmail.com>
2022-05-06 00:27:07 +0200

    epic win

--- src/screens/Records/Records.jsx
+++ src/screens/Records/Records.jsx
@@ -10,23 +10,28 @@ const Records = () => {
     const [records, setRecords] = useState([]);
 
     const b64toBlob = (base64, type = 'application/octet-stream') =>
-        fetch(`data:${type};base64,${base64}`).then(res => res.blob())
+        fetch(`${base64}`).then(res => res.blob())
 
     let getRecords = async () => {
         let response = await axios.get('http://localhost:3000/records')
-        let records = response.data
+        let recordsR = response.data
 
-        console.log('records:', records)
+        console.log('records:', recordsR)
 
-        let recordsAsMp3 = records.map(async r => {
+        let recordsAsMp3 = await Promise.all(recordsR.map(async r => {
             console.log('converting to blob', r)
 
             let blob = await b64toBlob(r.buffer)
+            console.log('converted', blob)
+            let url = window.URL.createObjectURL(blob)
+            console.log(url)
             return {
                 filename: r.filename,
-                blob: blob
+                blob: url
             }
-        })
+        }))
+
+        console.log('recordsAsMp3', recordsAsMp3)
 
         setRecords(recordsAsMp3)
     }
@@ -35,16 +40,24 @@ const Records = () => {
         getRecords();
     }, []);
 
-    let recordsElems = records.map(r => (
-        <tr>
-            <th scope="row "></th>
-            <td>{r.filename}</td>
-            <td>BOUTON PLAY</td>
-            <td>DELETE</td>
-            <td><a href={r.blob}>DOWNLOAD</a></td>
-            <td>CONTACT</td>
-        </tr>
-    ))
+    let recordsElems = records.map(r => {
+        console.log('r', r);
+        return (
+
+            <tr>
+                <th scope="row "></th>
+                <td>{r.filename}</td>
+                <td>BOUTON PLAY</td>
+                <td>DELETE</td>
+                <td>
+                    <audio controls="controls">
+                        <source src={r.blob} type="audio/mp3" />
+                    </audio>
+                </td>
+                <td>CONTACT</td>
+            </tr>
+        )
+    })
 
     return (
         <div>
