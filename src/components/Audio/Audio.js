7c6b20e3c4981dde0fbe51d54160037bc1047d5b
benoitgaudin <benoitgaudin44@gmail.com>
2022-05-16 20:39:17 +0200

    refacto

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -9,7 +9,6 @@ const Audio = () => {
     const [isRecording, setIsRecording] = useState(false);
     const [blobURL, setBlobURL] = useState('');
     const [authorize, setAuthorize] = useState(true);
-    const [isRecordingStp, setIsRecordingStp] = useState(false);
 
     useEffect(() => {
         navigator.getUserMedia = (
@@ -20,77 +19,78 @@ const Audio = () => {
         );
 
         navigator.getUserMedia({ audio: true },
-
             () => { setAuthorize(true) },
-
             () => { setAuthorize(false) }
         );
 
     }, []);
 
     const start = () => {
-
         if (!authorize) {
             alert('Permission Denied');
         } else {
             Mp3Recorder
                 .start()
-                .then(() => {
-                    setIsRecording(true)
-                }).catch((e) => console.error(e));
+                .then(() => setIsRecording(true))
+                .catch((e) => console.error(e));
         }
     };
-    const stop = () => {
-        let blobToBase64 = (blob) => {
-            return new Promise((resolve) => {
-                const reader = new FileReader();
-                reader.readAsDataURL(blob);
-                reader.onloadend = function () {
-                    resolve(reader.result);
-                };
-            });
-        };
-        let storeRecord = (filename, buffer) => {
-            return axios.post('http://localhost:3000/record', {
-                filename: filename,
-                buffer: buffer
-            })
-        }
+
+    const stop = () =>
         Mp3Recorder
             .stop()
             .getMp3()
-            .then(([buffer, blob]) => {
-                const blobURL = URL.createObjectURL(blob)
-                setBlobURL(blobURL)
+            .then(([_, blob]) => {
+                showRecordInView(blob)
                 setIsRecording(false)
-                setIsRecordingStp(true)
-                return blobToBase64(blob)
+                return blob
             })
-            .then(b64 => {
-                const today = new Date();
+            .then(encodeAndStore)
+            .catch(e => console.log(e));
 
-                let body = { filename: `${today.toISOString()}.mp3`, buffer: b64 }
-                JSON.stringify(body);
-                return storeRecord(body.filename, body.buffer)
-            })
-            .catch((e) => console.log(e));
-    };
-    const reset = () => {
+    const reset = () => setBlobURL('');
 
-        document.getElementsByTagName('audio')[0].src = '';
-        setIsRecordingStp(false)
-    };
+    function showRecordInView(blob) {
+        const blobURL = URL.createObjectURL(blob)
+        setBlobURL(blobURL)
+    }
+
     return (
-
         <div className="row d-flex justify-content-center mt-5 ">
             <div className="buttonRecord">
                 <button className="btn btn-light border-dark" onClick={start} disabled={isRecording}>RECORD</button></div>
             <div className="buttonRecord">
                 <button className="btn btn-light border-dark" onClick={stop} disabled={!isRecording}>STOP</button></div>
             <div className="buttonRecord">
-                <button className="btn btn-light border-dark" onClick={reset} disabled={!isRecordingStp}>RESET</button></div>
+                <button className="btn btn-light border-dark" onClick={reset} disabled={!(blobURL != '')}>RESET</button></div>
             <audio src={blobURL} controls="controls" />
         </div >
     )
 }
+
+function encodeAndStore(blob) {
+    const filename = `${new Date().toISOString()}.mp3`;
+    blobToBase64(blob).then(b64 => {
+        let body = { filename: filename, buffer: b64 }
+        return storeRecord(body.filename, body.buffer)
+    })
+}
+
+let blobToBase64 = (blob) => {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+            resolve(reader.result);
+        };
+    });
+};
+
+let storeRecord = (filename, buffer) => {
+    return axios.post('http://localhost:3000/record', {
+        filename: filename,
+        buffer: buffer
+    })
+}
+
 export default Audio
